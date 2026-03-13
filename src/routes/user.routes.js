b2269d53afb1e24ceae249e4fd6b13468d42bc94
Gaurav Kumar <gk107174@gmail.com>
2026-03-13 22:10:38 +0530

    Refactor user routes: organize imports and update refresh token route to use hyphen

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword,
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverimg, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js";
 import  {upload} from "../middleware/multer.js";
 import {verifyJWT} from "../middleware/auth.js"
 
@@ -23,6 +35,14 @@ router.route("/login").post(loginUser);
 //secure routes
 router.route("/logout").post(verifyJWT,logoutUser);
 
-router.route("/refreshtoken").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-img").patch(verifyJWT, upload.single("coverImage"), updateUserCoverimg)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //params
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router ;
